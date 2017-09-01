6cf27210252709c2a3a600b8f107fb970ef5e879
ouweiming <ouweiming.yeah.net>
2017-09-01 17:52:26 +0800

    Initial commit

---
+++ index.py
@@ -0,0 +1,57 @@
+
+
+# python 3.x 开始 print() 是一个方法， 2.x之前 print ""  直接输入
+print("Hello word!")
+
+
+# python可以直接复制，不用指定数据类型
+counter = 100
+miles = 1000.0
+name = "John"
+
+print(counter)
+print(miles)
+print(name)
+
+# 字符串可以用类似数组的形式来时直接输出里面的元素，下标从0开始，
+# 也可以用 name[1:3] 来截取 第二个到第三个来形成新的字符串
+print(name[1:3])
+
+# 也可以用 name[1:] 来截取从下标为一的元素直到最后一个元素的字符串
+print(name[1:])
+
+
+# List
+# python 可以直接定义不同数据类型的列表
+user = {'Trry', 123, 3.14, 'ocean'}
+# python 可以使用print() 直接输出列表的内容，但是不是按照顺序的
+print(user)
+# 使用 user[0] 可以使用下标来获取对应的元素
+print(user[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
